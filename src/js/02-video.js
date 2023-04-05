d0c4dd38b4985fcd39165726ea876f5c9986e242
Adrian Wojtczak <117528537+adrianwojtczak@users.noreply.github.com>
2023-04-05 23:30:15 +0200

    update task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,10 @@
 // Import libraries
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 // Add Vimeo Player
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const throttle = require('lodash.throttle');
 
 // Add lisener for timeupdate
 player.on(
@@ -12,8 +12,7 @@ player.on(
   throttle(function (data) {
     const currentTime = data.seconds;
     localStorage.setItem('videoplayer-current-time', currentTime);
-  }),
-  1000
+  }, 1000)
 );
 
 // Resume video
